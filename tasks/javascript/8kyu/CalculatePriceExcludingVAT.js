d71882840386a2de0ebe0d02147ad603d8dc5ed4
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-01 21:26:06 +0300

    Calculate Price Excluding VAT

---
+++ tasks/javascript/8kyu/CalculatePriceExcludingVAT.js
@@ -0,0 +1,30 @@
+// Calculate Price Excluding VAT
+
+// Write a function that calculates the original product price, without VAT.
+
+// Example
+// If a product price is 200.00 and VAT is 15%, then the final product price (with VAT) is: 200.00 + 15% = 230.00
+
+// Thus, if your function receives 230.00 as input, it should return 200.00
+
+// Notes:
+// VAT is always 15% for the purposes of this Kata.
+// Round the result to 2 decimal places.
+// If null value given then return -1
+
+// Sample Tests:
+
+// describe('Fixed Tests', () => {
+//     it("Test", () => {
+//       Test.assertSimilar(excludingVatPrice(230), 200.00);
+//       Test.assertSimilar(excludingVatPrice(123), 106.96);
+//     });
+//   });
+
+function excludingVatPrice(price) {
+  if (price == null) {
+    return -1;
+  }
+
+  return +(price / 1.15).toFixed(2);
+}
