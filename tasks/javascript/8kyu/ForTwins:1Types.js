648a34820ac2fdf0fc56f5bed044e49ff32a1460
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-05 16:22:38 +0300

    For Twins: 1. Types

---
+++ tasks/javascript/8kyu/ForTwins:1Types.js
@@ -0,0 +1,24 @@
+// For Twins: 1. Types
+
+// Prolog:
+
+// This kata series was created for friends of mine who just started to learn programming. Wish you all the best and keep your mind open and sharp!
+
+// Task:
+
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+
+// 42, "number"   --> true
+// "42", "number" --> false
+
+// Sample Tests:
+// describe("Basic Tests", function() {
+//     Test.assertEquals(typeValidation(42, "number"), true);
+//     Test.assertEquals(typeValidation("42", "number"), false);
+//   });
+
+function typeValidation(variable, type) {
+  return typeof variable === type;
+}
