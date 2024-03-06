2aa9677bd715371de09f9dd5c70ed7d67c81e059
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-07 01:45:46 +0300

    Predict your age!

---
+++ tasks/javascript/7kyu/PredictYourAge!.js
@@ -0,0 +1,34 @@
+// Predict your age!
+
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+// Example
+
+// predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+// Note: the result should be rounded down to the nearest integer.
+
+// Some random tests might fail due to a bug in the JavaScript implementation. Simply resubmit if that happens to you.
+
+// Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.strictEqual(predictAge(65,60,75,55,60,63,64,45), 86);
+//   });
+// })
+
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  let ages = [age1, age2, age3, age4, age5, age6, age7, age8];
+  let sum = ages.reduce((acc, age) => acc + age * age, 0);
+  return Math.floor(Math.sqrt(sum) / 2);
+}
