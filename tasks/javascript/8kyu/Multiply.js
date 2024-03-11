416f08006f49ccec78a05de7458eb4ea4074ba9b
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-11 14:40:07 +0200

    Multiply

---
+++ tasks/javascript/8kyu/Multiply.js
@@ -0,0 +1,22 @@
+// Multiply
+
+// This code does not execute properly. Try to figure out why.
+
+// Sample Tests:
+// const assert = require("chai").assert;
+
+// describe("Multiply", () => {
+//   it("fixed tests", () => {
+//     assert.strictEqual(multiply(1,1), 1);
+//     assert.strictEqual(multiply(2,1), 2);
+//     assert.strictEqual(multiply(2,2), 4);
+//     assert.strictEqual(multiply(3,5), 15);
+//     assert.strictEqual(multiply(5,0), 0);
+//     assert.strictEqual(multiply(0,5), 0);
+//     assert.strictEqual(multiply(0,0), 0);
+//   });
+// });
+
+function multiply(a, b) {
+  return a * b;
+}
