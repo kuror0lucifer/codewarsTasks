0bef11f4db31d445961e5b067b7ea14db3d30a47
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-11 14:38:44 +0200

    Multi Line Task++: Hello World

---
+++ tasks/javascript/2kyu/MultiLineTask++:HelloWorld.js
@@ -0,0 +1,55 @@
+// Multi Line Task++: Hello World
+
+// You need to write a function f that returns the string Hello, world!.
+
+// Requirement: Every line must have at most 2 characters, and total number of lines must be less than 40.
+
+// Hint: It's possible to complete this in 28 lines only.
+
+// Sample Tests:
+// describe("Basic tests", function(){
+//     it("It should work for basic tests", function(){
+//       Test.assertEquals(f(),'Hello, world!');
+//     });
+//   });
+
+//   describe("Code length check", function(){
+//     it("Every line should have less than 3 characters", function(){
+//       const limit = 3;
+//       const lines = usercode.split('\n');
+//       Test.expect(Math.max(...lines.map(s=>s.length))<limit, `Some lines of your code exceeded the character limit`);
+//     });
+//     it("There should be less than 40 lines", function(){
+//       const limit = 40;
+//       const lines = usercode.split('\n');
+//       Test.expect(lines.length<limit, `Your code has ${lines.length} lines`);
+//     });
+//   });
+
+f = ""[
+  "\
+t\
+r\
+i\
+m"
+][
+  "\
+b\
+i\
+n\
+d"
+]`
+H\
+e\
+l\
+l\
+o\
+,\
+ \
+w\
+o\
+r\
+l\
+d\
+!\
+`;
