f4494d809973c31239691b95b3ccced016ea14b1
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-22 21:59:38 +0200

    Remove String Spaces

---
+++ tasks/javascript/8kyu/RemoveStringSpaces.js
@@ -0,0 +1,27 @@
+// Remove String Spaces
+
+// Write a function that removes the spaces from the string, then return the resultant string.
+
+// Examples:
+
+// Input -> Output
+// "8 j 8   mBliB8g  imjB8B8  jl  B" -> "8j8mBliB8gimjB8B8jlB"
+// "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" -> "88Bifk8hB8BB8BBBB888chl8BhBfd"
+// "8aaaaa dddd r     " -> "8aaaaaddddr"
+
+// Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests",() =>{
+//   it("Fixed Tests", () => {
+//     assert.strictEqual(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB');
+//     assert.strictEqual(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd');
+//     assert.strictEqual(noSpace('8aaaaa dddd r     '), '8aaaaaddddr');
+//   });
+// });
+
+function noSpace(x) {
+  return x.split(" ").join("");
+}
