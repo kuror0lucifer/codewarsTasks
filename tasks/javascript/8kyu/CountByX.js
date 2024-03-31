04a21386d81a7ab2a8ff2a4b1ecc75abe43f5bb8
kuror0lucifer <kovalishin.2014@mail.ru>
2024-03-31 22:45:13 +0200

    Count by X

---
+++ tasks/javascript/8kyu/CountByX.js
@@ -0,0 +1,32 @@
+// Count by X
+
+// Create a function with two arguments that will return an array of the first n multiples of x.
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array or list ( depending on language ).
+
+// Examples
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+// Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic Tests",function(){
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(countBy(1,10), [1,2,3,4,5,6,7,8,9,10], "Array does not match")
+//     assert.deepEqual(countBy(2,5), [2,4,6,8,10], "Array does not match")
+//   });
+// });
+
+function countBy(x, n) {
+  let z = [];
+  for (let i = x; i <= x * n; i += x) {
+    z.push(i);
+  }
+  return z;
+}
