2cf74664989ae7a03a1fa620ec543a422c05419c
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-09 21:25:03 +0200

    Returning Strings

---
+++ tasks/typescript/8kyu/ReturningStrings.ts
@@ -0,0 +1,20 @@
+// Returning Strings
+
+// Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+// [Make sure you type the exact thing I wrote or the program may not execute properly]
+
+// Sample Tests:
+// import { assert } from "chai";
+// import { greet } from "./solution";
+
+// describe("Basic tests", () => {
+//   it("Should pass basic tests", () => {
+//     assert.equal(greet("Ryan"), "Hello, Ryan how are you doing today?");
+//     assert.equal(greet("Shingles"), "Hello, Shingles how are you doing today?");
+//   });
+// });
+
+export function greet(name: string): string {
+  return `Hello, ${name} how are you doing today?`;
+}
