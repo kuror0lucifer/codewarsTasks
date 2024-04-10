37a7a1a2646dbd65e84daf4d90fdd977df3685d1
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-10 22:06:48 +0200

    You Can't Code Under Pressure #1

---
+++ tasks/typescript/8kyu/YouCantCodeUnderPressure.ts
@@ -0,0 +1,17 @@
+// You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+// Sample Tests:
+// import { assert } from "chai";
+// import { doubleInteger } from "./solution";
+
+// describe("Double integer", () => {
+//   it("Fixed tests", () => {
+//     assert.strictEqual(doubleInteger(2), 4);
+//   });
+// });
+
+export function doubleInteger(i: number): number {
+  return i + i;
+}
