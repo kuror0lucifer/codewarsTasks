81524ba74831d27f0b74f39881e2370728baac7a
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-11 17:06:16 +0200

    Function 1 - hello world

---
+++ tasks/typescript/8kyu/Function1HelloWorld.ts
@@ -0,0 +1,16 @@
+// Function 1 - hello world
+
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
+
+// Sample Tests:
+// import { assert } from "chai";
+// import { greet } from "./solution";
+
+// describe("hello world", () => it("greeting", () => assert.equal(greet(), "hello world!")));
+
+export function greet(): string {
+  return "hello world!";
+}
