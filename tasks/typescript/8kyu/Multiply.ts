5db277332295f1c8a31300554e99c6bf8ca5ce32
kuror0lucifer <kovalishin.2014@mail.ru>
2024-04-11 16:59:51 +0200

    Multiply

---
+++ tasks/typescript/8kyu/Multiply.ts
@@ -0,0 +1,18 @@
+// Multiply
+
+// This code does not execute properly. Try to figure out why.
+
+// Sample Tests:
+// import { multiply } from './solution';
+// import { assert } from "chai";
+
+// describe("multiply", function() {
+//   it("should return the product", () => {
+//     assert.strictEqual(multiply(1, 1), 1);
+//     assert.strictEqual(multiply(1, 2), 2);
+//   });
+// });
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
