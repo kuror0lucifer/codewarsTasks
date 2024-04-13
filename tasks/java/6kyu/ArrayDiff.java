0665c789f06ca20f4321d86249aaf9ee7e7ce863
batru <ba.baturin2016@yandex.ru>
2024-04-13 14:32:38 +0200

    Array.diff

--- tasks/java/6kyu/ArrayDiff.java
+++ tasks/java/6kyu/ArrayDiff.java
@@ -11,21 +11,31 @@ import java.util.List;
 public class ArrayDiff {
     public static int[] arrayDiff(int[] a, int[] b) {
         List<Integer> resultList = new ArrayList<>();
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < b.length; j++) {
-                if (a[i] != b[j]) {
-                    resultList.add(a[i]);
-                }
+        
+        // Преобразование массива b в список для удобства проверки наличия элементов
+        List<Integer> bList = new ArrayList<>();
+        for (int num : b) {
+            bList.add(num);
+        }
+        
+        // Проверка каждого элемента массива a
+        for (int num : a) {
+            // Если элемента нет в списке b, добавляем его в результат
+            if (!bList.contains(num)) {
+                resultList.add(num);
             }
         }
-        int [] resArr = new int[resultList.size()];
+        
+        // Преобразование списка обратно в массив
+        int[] resultArray = new int[resultList.size()];
         for (int i = 0; i < resultList.size(); i++) {
-            resArr[i] = resultList.get(i);
+            resultArray[i] = resultList.get(i);
         }
-        return resArr;
+        
+        return resultArray;
     }
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(arrayDiff(new int[]{1,2,2,3}, new int[]{2})));
+        System.out.println(Arrays.toString(arrayDiff(new int[]{1,2,2}, new int[]{1,2,2})));
     }    
 }
 
