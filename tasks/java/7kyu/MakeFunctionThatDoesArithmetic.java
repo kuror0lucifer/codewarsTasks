9700fb4fea6f5b9211f60a9c062178c2a7c6df64
batru <ba.baturin2016@yandex.ru>
2024-04-17 11:02:31 +0200

    Make a function that does arithmetic!

--- tasks/java/7kyu/MakeFunctionThatDoesArithmetic.java
+++ tasks/java/7kyu/MakeFunctionThatDoesArithmetic.java
@@ -12,7 +12,6 @@
 // import static org.junit.Assert.assertEquals;
 // import org.junit.runners.JUnit4;
 
-// // TODO: Replace examples and use TDD development by writing your own tests
 
 // public class SolutionTest {
 //     @Test
