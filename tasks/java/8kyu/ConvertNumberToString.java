288b6484dda7c3fe8c4b331837c5ffed42fc87da
batru <ba.baturin2016@yandex.ru>
2024-04-17 09:37:23 +0200

    Convert a Number to a String

---
+++ tasks/java/8kyu/ConvertNumberToString.java
@@ -0,0 +1,16 @@
+// Convert a Number to a String
+
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+// Examples (input --> output):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+public class ConvertNumberToString {
+    public static String numberToString(int num) {
+        return Integer.toString(num);
+    }
+}
