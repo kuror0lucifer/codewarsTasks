440a233b91d4d45ada90a04a098bd4580cd52c69
Alexandr <124583216+Little5quad@users.noreply.github.com>
2024-04-17 14:17:01 +0300

    Create Twice_as_old.py

---
+++ tasks/python/8kyu/Twice_as_old.py
@@ -0,0 +1,13 @@
+# Your function takes two arguments:
+
+#current father's age (years)
+#current age of his son (years)
+#Сalculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old). 
+#The answer is always greater or equal to 0, no matter if it was in the past or it is in the future.
+
+def twice_as_old(dad_years_old, son_years_old):
+    twice_ = (dad_years_old - son_years_old) * 2
+    if dad_years_old >= twice_:
+        return dad_years_old - twice_
+    else:
+        return twice_ - dad_years_old
