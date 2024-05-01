3d5663c02e862ed4e7a323e785db70952ea651c5
kuror0lucifer <kovalishin.2014@mail.ru>
2024-05-01 19:08:14 +0300

    Get the Middle Character

---
+++ tasks/typescript/7kyu/GetTheMiddleCharacter.ts
@@ -0,0 +1,34 @@
+// Get the Middle Character
+
+// You are going to be given a word. Your job is to return the middle character of the word. If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+// #Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+// #Input
+
+// A word (string) of length 0 < str < 1000 (In javascript you may get slightly more than 1000 in some test cases due to an error in the test cases). You do not need to test for this. This is only here to tell you that you do not need to worry about your solution timing out.
+
+// #Output
+
+// The middle character(s) of the word represented as a string.
+
+// Sample Tests:
+// import {getMiddle} from './solution'
+// import {assert} from "chai"
+
+// function test(string:string, expected:string) {
+//   assert.strictEqual(getMiddle(string), expected)
+// }
+
+export function getMiddle(s: string) {
+  return !(s.length % 2)
+    ? s.substring(Math.floor(s.length / 2) - 1, Math.floor(s.length / 2) + 1)
+    : s[Math.floor(s.length / 2)];
+}
