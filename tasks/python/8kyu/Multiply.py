1fe843b4d4c2d0ca74d560db0424c110d196c399
kuror0lucifer <kovalishin.2014@mail.ru>
2024-05-17 20:40:32 +0200

    Multiply

---
+++ tasks/python/8kyu/Multiply.py
@@ -0,0 +1,19 @@
+# Multiply
+
+# This code does not execute properly. Try to figure out why.
+
+# Sample Tests:
+# import codewars_test as test
+# from solution import multiply
+
+# @test.describe("Fixed Tests")
+# def fixed_tests():
+#     @test.it('Basic Test Cases')
+#     def basic_test_cases():
+#         test.assert_equals(multiply(2,1), 2)
+#         test.assert_equals(multiply(5,0), 0)
+#         test.assert_equals(multiply(0,5), 0)
+#         test.assert_equals(multiply(0,0), 0)
+        
+def multiply(a, b):
+    return a * b
