007b61a8c6d3a4e2f30ab36d1ae696a3ff1a582d
kuror0lucifer <kovalishin.2014@mail.ru>
2024-05-29 21:57:12 +0200

    Discover The Original Price

---
+++ tasks/javascript/7kyu/DiscoverTheOriginalPrice.js
@@ -0,0 +1,27 @@
+// Discover The Original Price
+
+// We need to write some code to return the original price of a product, the return type must be of type decimal and the number must be rounded to two decimal places.
+
+// We will be given the sale price (discounted price), and the sale percentage, our job is to figure out the original price.
+
+// For example:
+// Given an item at $75 sale price after applying a 25% discount, the function should return the original price of that item before applying the sale percentage, which is ($100.00) of course, rounded to two decimal places.
+
+// DiscoverOriginalPrice(75, 25) => 100.00M where 75 is the sale price (discounted price), 25 is the sale percentage and 100 is the original price
+
+// Sample Tests:
+// const Test = require('@codewars/test-compat');
+
+// describe("Tests", () => {
+//   it("test", () => {
+// Test.assertEquals(discoverOriginalPrice(75,25),100);
+// Test.assertEquals(discoverOriginalPrice(25,75),100);
+// Test.assertEquals(discoverOriginalPrice(75.75,25),101);
+
+//   });
+// });
+
+function discoverOriginalPrice(discountedPrice, salePercentage) {
+  const originalPrice = discountedPrice / (1 - salePercentage / 100);
+  return parseFloat(originalPrice.toFixed(2));
+}
