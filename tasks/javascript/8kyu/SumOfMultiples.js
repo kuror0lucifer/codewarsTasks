6433fa20ebd1fb8ffd9356df35c1646aceb9fa54
kuror0lucifer <kovalishin.2014@mail.ru>
2024-05-30 20:21:01 +0200

    Sum of Multiples

---
+++ tasks/javascript/8kyu/SumOfMultiples.js
@@ -0,0 +1,33 @@
+// Sum of Multiples
+
+// Your Job
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+// Sample Tests:
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Tests", () => {
+//   it("should test", () => {
+//     assert.strictEqual(sumMul(0,0),"INVALID");
+//     assert.strictEqual(sumMul(2,9),20);
+//     assert.strictEqual(sumMul(4,-7),"INVALID");
+//   });
+// });
+
+function sumMul(n, m) {
+  if (n <= 0 || m <= 0 || m <= n) return 'INVALID';
+  let sum = 0;
+  for (let i = n; i < m; i += n) sum += i;
+  return sum;
+}
