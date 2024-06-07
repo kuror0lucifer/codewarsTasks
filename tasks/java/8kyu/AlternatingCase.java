e669579ac4c3fb79ae5dd4ebd52f5730630bd33b
PampersKrut <ba.baturin2016@yandex.ru>
2024-06-07 09:37:54 +0200

    altERnaTIng cAsE <=> ALTerNAtiNG CaSe

---
+++ tasks/java/8kyu/AlternatingCase.java
@@ -0,0 +1,34 @@
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// StringUtils.toAlternativeString("hello world") == "HELLO WORLD"
+// StringUtils.toAlternativeString("HELLO WORLD") == "hello world"
+// StringUtils.toAlternativeString("hello WORLD") == "HELLO world"
+// StringUtils.toAlternativeString("HeLLo WoRLD") == "hEllO wOrld"
+// StringUtils.toAlternativeString("12345") == "12345" // Non-alphabetical characters are unaffected
+// StringUtils.toAlternativeString("1a2b3c4d5e") == "1A2B3C4D5E"
+// StringUtils.toAlternativeString("StringUtils.toAlternatingCase") == "sTRINGuTILS.TOaLTERNATINGcASE"
+
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
+public class AlternatingCase {
+    public static String toAlternativeString(String string) {
+        char[] arr = string.toCharArray();
+        StringBuilder builder = new StringBuilder();
+        for (Character charArr : arr) {
+            if (charArr == Character.toLowerCase(charArr) ) {
+                builder.append(Character.toUpperCase(charArr));
+            }
+            if (charArr != Character.toLowerCase(charArr)) {
+                builder.append(Character.toLowerCase(charArr));
+            }
+        }
+        return builder.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(toAlternativeString("hello WorlD"));
+    }
+}
